1b4ee087d3e853a36d625372256e6894a4e81651
Nain Lobato Garcia <wolverinenain@gmail.com>
2018-03-30 20:44:12 -0600

    Prototipo de plantilla Admin LTE v3.

---
+++ resources/views/template/partials/footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+	<!-- To the right -->
+	<div class="float-right d-sm-none d-md-block">
+		Develop by QuarkCode
+	</div>
+	<!-- Default to the left -->
+	<strong>Copyright &copy; 2018.</strong> All rights reserved.
+</footer>

a9b2cc5af83dbd238cc6419e245529b6af77e85a
Nain Lobato Garcia <wolverinenain@gmail.com>
2018-03-30 22:21:42 -0600

    Plantilla AdminLTE al 70%.

--- resources/views/template/partials/navbar.blade.php
+++ resources/views/template/partials/navbar.blade.php
@@ -5,17 +5,17 @@
 			<a class="nav-link" data-widget="pushmenu" href="#"><i class="fa fa-bars"></i></a>
 		</li>
 		<li class="nav-item d-none d-sm-inline-block">
-			<a href="index3.html" class="nav-link">Home</a>
+			<a href="#" class="nav-link">Inicio</a>
 		</li>
 		<li class="nav-item d-none d-sm-inline-block">
-			<a href="#" class="nav-link">Contact</a>
+			<a href="#" class="nav-link">Contacto</a>
 		</li>
 	</ul>
 
 	<!-- SEARCH FORM -->
 	<form class="form-inline ml-3">
 		<div class="input-group input-group-sm">
-			<input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
+			<input class="form-control form-control-navbar" type="search" placeholder="Buscar" aria-label="Search">
 			<div class="input-group-append">
 				<button class="btn btn-navbar" type="submit">
 					<i class="fa fa-search"></i>
@@ -36,7 +36,7 @@
 				<a href="#" class="dropdown-item">
 					<!-- Message Start -->
 					<div class="media">
-						<img src="dist/img/user1-128x128.jpg" alt="User Avatar" class="img-size-50 mr-3 img-circle">
+						<img src="{{ asset('admin/dist/img/user1-128x128.jpg') }}" alt="User Avatar" class="img-size-50 mr-3 img-circle">
 						<div class="media-body">
 							<h3 class="dropdown-item-title">
 								Brad Diesel
@@ -52,7 +52,7 @@
 				<a href="#" class="dropdown-item">
 					<!-- Message Start -->
 					<div class="media">
-						<img src="dist/img/user8-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">
+						<img src="{{ asset('admin/dist/img/user8-128x128.jpg') }}" alt="User Avatar" class="img-size-50 img-circle mr-3">
 						<div class="media-body">
 							<h3 class="dropdown-item-title">
 								John Pierce
@@ -68,7 +68,7 @@
 				<a href="#" class="dropdown-item">
 					<!-- Message Start -->
 					<div class="media">
-						<img src="dist/img/user3-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">
+						<img src="{{ asset('admin/dist/img/user3-128x128.jpg') }}" alt="User Avatar" class="img-size-50 img-circle mr-3">
 						<div class="media-body">
 							<h3 class="dropdown-item-title">
 								Nora Silvester
@@ -84,6 +84,7 @@
 				<a href="#" class="dropdown-item dropdown-footer">See All Messages</a>
 			</div>
 		</li>
+
 		<!-- Notifications Dropdown Menu -->
 		<li class="nav-item dropdown">
 			<a class="nav-link" data-toggle="dropdown" href="#">
@@ -112,8 +113,7 @@
 			</div>
 		</li>
 		<li class="nav-item">
-			<a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#"><i
-					class="fa fa-th-large"></i></a>
+			<a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#"><i class="fa fa-th-large"></i></a>
 		</li>
 	</ul>
 </nav>
--- resources/views/template/partials/sidebar.blade.php
+++ resources/views/template/partials/sidebar.blade.php
@@ -1,9 +1,8 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
 	<!-- Brand Logo -->
-	<a href="index3.html" class="brand-link">
-		<img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
-				 style="opacity: .8">
-		<span class="brand-text font-weight-light">AdminLTE 3</span>
+	<a href="#" class="brand-link">
+		<img src="{{ asset('admin/dist/img/AdminLTELogo.png') }}" alt="Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+		<span class="brand-text font-weight-light">Limones Mónica</span>
 	</a>
 
 	<!-- Sidebar -->
@@ -11,7 +10,7 @@
 		<!-- Sidebar user panel (optional) -->
 		<div class="user-panel mt-3 pb-3 mb-3 d-flex">
 			<div class="image">
-				<img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+				<img src="{{ asset('admin/dist/img/user2-160x160.jpg') }}" class="img-circle elevation-2" alt="Imagen de perfil">
 			</div>
 			<div class="info">
 				<a href="#" class="d-block">Alexander Pierce</a>
@@ -33,27 +32,21 @@
 					</a>
 					<ul class="nav nav-treeview">
 						<li class="nav-item">
-							<a href="./index.html" class="nav-link">
+							<a href="#" class="nav-link">
 								<i class="fa fa-circle-o nav-icon"></i>
 								<p>Dashboard v1</p>
 							</a>
 						</li>
 						<li class="nav-item">
-							<a href="./index2.html" class="nav-link active">
+							<a href="#" class="nav-link active">
 								<i class="fa fa-circle-o nav-icon"></i>
 								<p>Dashboard v2</p>
-							</a>
-						</li>
-						<li class="nav-item">
-							<a href="./index3.html" class="nav-link">
-								<i class="fa fa-circle-o nav-icon"></i>
-								<p>Dashboard v3</p>
 							</a>
 						</li>
 					</ul>
 				</li>
 				<li class="nav-item">
-					<a href="pages/widgets.html" class="nav-link">
+					<a href="#" class="nav-link">
 						<i class="nav-icon fa fa-th"></i>
 						<p>
 							Widgets
@@ -71,19 +64,19 @@
 					</a>
 					<ul class="nav nav-treeview">
 						<li class="nav-item">
-							<a href="pages/charts/chartjs.html" class="nav-link">
+							<a href="#" class="nav-link">
 								<i class="fa fa-circle-o nav-icon"></i>
 								<p>ChartJS</p>
 							</a>
 						</li>
 						<li class="nav-item">
-							<a href="pages/charts/flot.html" class="nav-link">
+							<a href="#" class="nav-link">
 								<i class="fa fa-circle-o nav-icon"></i>
 								<p>Flot</p>
 							</a>
 						</li>
 						<li class="nav-item">
-							<a href="pages/charts/inline.html" class="nav-link">
+							<a href="#" class="nav-link">
 								<i class="fa fa-circle-o nav-icon"></i>
 								<p>Inline</p>
 							</a>
@@ -100,25 +93,25 @@
 					</a>
 					<ul class="nav nav-treeview">
 						<li class="nav-item">
-							<a href="pages/UI/general.html" class="nav-link">
+							<a href="#" class="nav-link">
 								<i class="fa fa-circle-o nav-icon"></i>
 								<p>General</p>
 							</a>
 						</li>
 						<li class="nav-item">
-							<a href="pages/UI/icons.html" class="nav-link">
+							<a href="#" class="nav-link">
 								<i class="fa fa-circle-o nav-icon"></i>
 								<p>Icons</p>
 							</a>
 						</li>
 						<li class="nav-item">
-							<a href="pages/UI/buttons.html" class="nav-link">
+							<a href="#" class="nav-link">
 								<i class="fa fa-circle-o nav-icon"></i>
 								<p>Buttons</p>
 							</a>
 						</li>
 						<li class="nav-item">
-							<a href="pages/UI/sliders.html" class="nav-link">
+							<a href="#" class="nav-link">
 								<i class="fa fa-circle-o nav-icon"></i>
 								<p>Sliders</p>
 							</a>
@@ -135,19 +128,19 @@
 					</a>
 					<ul class="nav nav-treeview">
 						<li class="nav-item">
-							<a href="pages/forms/general.html" class="nav-link">
+							<a href="#" class="nav-link">
 								<i class="fa fa-circle-o nav-icon"></i>
 								<p>General Elements</p>
 							</a>
 						</li>
 						<li class="nav-item">
-							<a href="pages/forms/advanced.html" class="nav-link">
+							<a href="#" class="nav-link">
 								<i class="fa fa-circle-o nav-icon"></i>
 								<p>Advanced Elements</p>
 							</a>
 						</li>
 						<li class="nav-item">
-							<a href="pages/forms/editors.html" class="nav-link">
+							<a href="#" class="nav-link">
 								<i class="fa fa-circle-o nav-icon"></i>
 								<p>Editors</p>
 							</a>
@@ -164,19 +157,20 @@
 					</a>
 					<ul class="nav nav-treeview">
 						<li class="nav-item">
-							<a href="pages/tables/simple.html" class="nav-link">
+							<a href="#" class="nav-link">
 								<i class="fa fa-circle-o nav-icon"></i>
 								<p>Simple Tables</p>
 							</a>
 						</li>
 						<li class="nav-item">
-							<a href="pages/tables/data.html" class="nav-link">
+							<a href="#" class="nav-link">
 								<i class="fa fa-circle-o nav-icon"></i>
 								<p>Data Tables</p>
 							</a>
 						</li>
 					</ul>
 				</li>
+				{{--
 				<li class="nav-header">EXAMPLES</li>
 				<li class="nav-item">
 					<a href="pages/calendar.html" class="nav-link">
@@ -318,6 +312,7 @@
 						<p>Informational</p>
 					</a>
 				</li>
+				--}}
 			</ul>
 		</nav>
 		<!-- /.sidebar-menu -->
